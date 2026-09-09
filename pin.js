e46743ebb7334deeda418328aba202429906a512
JoshMcNab <jrjacksonthefirst@icloud.com>
2026-09-10 00:20:31 +0100

    Bump workshop frontend to v25.6

--- pin.js
+++ pin.js
@@ -1,5 +1,5 @@
 (()=>{
-  const VERSION='25.5';
+  const VERSION='25.6';
   const loaded=new Map();
 
   function asset(path){return `${path}?v=${VERSION}`}
@@ -27,7 +27,7 @@
     await loadScript('src/core/api-v25.js');
     await loadScript('src/core/auth-v25.js');
     await loadScript('src/core/features-v25.js');
-    document.documentElement.dataset.workshopVersion=window.WorkshopConfig?.version||'25.5';
+    document.documentElement.dataset.workshopVersion=window.WorkshopConfig?.version||'25.6';
   }
 
   boot().catch(error=>{
