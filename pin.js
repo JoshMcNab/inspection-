1aa732eaad0bbbf7bc554e022351e386881081cc
JoshMcNab <jrjacksonthefirst@icloud.com>
2026-09-10 00:10:10 +0100

    Bump workshop assets to v25.5

--- pin.js
+++ pin.js
@@ -1,5 +1,5 @@
 (()=>{
-  const VERSION='25.4';
+  const VERSION='25.5';
   const loaded=new Map();
 
   function asset(path){return `${path}?v=${VERSION}`}
@@ -27,7 +27,7 @@
     await loadScript('src/core/api-v25.js');
     await loadScript('src/core/auth-v25.js');
     await loadScript('src/core/features-v25.js');
-    document.documentElement.dataset.workshopVersion=window.WorkshopConfig?.version||'25.4';
+    document.documentElement.dataset.workshopVersion=window.WorkshopConfig?.version||'25.5';
   }
 
   boot().catch(error=>{
